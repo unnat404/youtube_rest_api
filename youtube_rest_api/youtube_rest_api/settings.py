1d513cf81c134ff5e6a0ecd757a3570d61cb7460
unnat <53619178+unnat404@users.noreply.github.com>
2021-12-19 18:57:53 +0530

    --youtube search view--

--- youtube_rest_api/youtube_rest_api/settings.py
+++ youtube_rest_api/youtube_rest_api/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_jsv0y4)d*!(0=^_duttklf460ll==jo^c@x094&ci)fxs!dv1'
+SECRET_KEY = config('SECRET_KEY') 
+YOUTUBE_DATA_API_KEY = config('YOUTUBE_DATA_API_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
